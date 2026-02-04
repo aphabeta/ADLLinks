74a578004cb9bbad9d3ee02f0beb205c134967c9
aphabeta <90509002+aphabeta@users.noreply.github.com>
2026-02-04 06:55:20 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-# Comma-separated Telegram user IDs
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "").split(",")))

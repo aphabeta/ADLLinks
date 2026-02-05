0f36ccf637074f1f01dc9e2b1d934a10746df325
aphabeta <90509002+aphabeta@users.noreply.github.com>
2026-02-05 20:44:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7420934118:AAHNtIkKsO-M-BxWO9PuWd1mdzA8eJHKlKk")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("mongodb+srv://linkstry:adrama123@cluster0.0se8pwx.mongodb.net/?appName=Cluster0")
 DB_NAME = "telegram_bot"
 
-FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL")  # @channelusername
+FORCE_JOIN_CHANNEL = os.getenv("https://t.me/IUsFiles")  # @channelusername

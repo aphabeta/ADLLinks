4b0e8707eebbd1866b8297999a510454ea8c7775
aphabeta <90509002+aphabeta@users.noreply.github.com>
2026-02-05 20:46:35 +0530

    Refactor environment variable usage in config.py
    Updated environment variable names for better security.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.getenv("7420934118:AAHNtIkKsO-M-BxWO9PuWd1mdzA8eJHKlKk")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-MONGO_URI = os.getenv("mongodb+srv://linkstry:adrama123@cluster0.0se8pwx.mongodb.net/?appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = "telegram_bot"
 
-FORCE_JOIN_CHANNEL = os.getenv("https://t.me/IUsFiles")  # @channelusername
+FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL")  # @channelusername

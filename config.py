f4a540ccb95da7e6f8888d5f9dba7d45c41635b0
aphabeta <90509002+aphabeta@users.noreply.github.com>
2026-02-05 23:29:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGODB_URI = os.getenv("MONGODB_URI")
 DB_NAME = "telegram_bot"
 
 FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL")  # @channelusername
